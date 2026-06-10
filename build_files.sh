410d40122af603ef8ea4adbd200512745163b108
arsh-code404 <arshushaikh120s@gmail.com>
2026-06-10 18:49:21 +0530

    Use --break-system-packages in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "=== Start Friday Originals Store Build ==="
 
-# Create and activate a virtual environment for the build phase
-python3 -m venv venv
-source venv/bin/activate
-
-# Install dependencies in the isolated virtual environment
-python3 -m pip install -r requirements.txt
+# Install dependencies using the system packages override since the build container is disposable
+python3 -m pip install -r requirements.txt --break-system-packages
 
 # Collect static files
 python3 manage.py collectstatic --noinput --clear
